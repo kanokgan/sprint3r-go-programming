ff5d70dfa6e1afdc7e24e7bb74b3dd3c1196635b
Kanokgan Hanmoungtham <kanokgan@gmail.com>
2015-08-22 10:25:16 +0700

    Variable type

--- hello.go
+++ hello.go
@@ -3,6 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	hello := "Hello World"
+	hello := "Hello World" // string
 	fmt.Println(hello)
+
+	var testInt int = 1 // int
+
+	var testBool bool = true // bool
+
+	var testfloat32 float32 = 1.0 // float32
+
+	var testfloat64 float64 = 1.0 // float64
+
+	var teststring string = "string" // string
+
+	var testunit uint = 1 // uint
+
+	var testint64 int64 = 1 // int64, long (in other language)
+
+	var testbyte byte = 1 // byte
 }
