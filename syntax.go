c02bc33c0647ba871c0c776d1d68ca998d36c0ac
Kanokgan Hanmoungtham <kanokgan@gmail.com>
2015-08-22 13:44:19 +0700

    Function

--- syntax.go
+++ syntax.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+
+// ตัวอย่างเรื่อง function
 func add(a, b int) int {
 	return a + b
 }
@@ -10,7 +12,28 @@ var addFunc func(int, int) int = func(a, b int) int {
 	return a + b
 }
 
+
+// ตัวอย่างสร้าง Framework เล็ก ๆ เอาเอง
+var router map[string]func(data string) = make(map[string]func(string))
+
+func SetRoute(route string, f func(data string)) {
+	router[route] = f
+}
+
+func RunRoute(route string, msg string) {
+	if f, ok := router[route]; ok {
+		f(msg)
+	}
+}
+
+
 func main() {
 	fmt.Println("10 + 20 =", addFunc(10, 20))
 	fmt.Println("20 + 30 =", add(20, 30))
+
+	SetRoute("/hello", func(msg string) {
+		fmt.Println("Hello", msg)
+	})
+
+	RunRoute("/hello", "Kanokgan")
 }
